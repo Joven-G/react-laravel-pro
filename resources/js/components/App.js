65ed18ff083cabfd79b67635575d5d4554cdf019
ballanse <devblackholes@gmail.com>
2022-04-06 00:19:43 +0900

    react Todo

--- resources/js/components/App.js
+++ resources/js/components/App.js
@@ -1,13 +1,24 @@
 import { Box } from "@mui/system";
-import React from "react";
+import React, { Component } from "react";
+
 import ReactDom from "react-dom";
 import Navigation from "./Nav/Navigation";
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./Home";
+
 
 function App() {
     return (
         <Box>
             <Navigation></Navigation>
-
+            <Router>
+                <div className="m-5">
+                    <Routes>
+                        <Route path="/" exact component={Home} />
+                    </Routes>
+                </div>
+            </Router>
         </Box>
     )
 }
---
+++ resources/js/components/Home.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDom from "react-dom";
+import { Grid } from "@mui/material";
+
+function Home() {
+    return (
+        <Grid container spacing={2}>
+            <Grid item xs={8}>
+                <div>hiroto</div>
+            </Grid>
+
+        </Grid>
+    )
+}
+
+export default Home;
