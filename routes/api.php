5f2b4ff4e9dbb20e571034b0d6b162db104af027
ballanse <devblackholes@gmail.com>
2022-04-08 20:23:20 +0900

    api準備

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\ToDoController;
+
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +19,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::resource('toDos', ToDoController::class);
+Route::resource('toDoDetails', ToDoDetailController::class);
